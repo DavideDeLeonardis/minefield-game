e43e39d45a33a02eea077f3f847ec1135b1ae9ef
Davide De Leonardis <davidedeleonardis1711@gmail.com>
2022-03-17 17:41:35 +0100

    add score, description, readme

---
+++ README.md
@@ -0,0 +1,2 @@
+## Simple game in JS, set the difficulty, click the play button and start the game!
+Bombs are 5 in easy mode, 10 in medium and 15 in impossible.
--- js/script.js
+++ js/script.js
@@ -1,86 +1,101 @@
-//     La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti. 
-//     Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
-// BONUS:
-// 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
-// 2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
-
 const difficulty = document.getElementById('difficulty');
 const buttonPlay = document.getElementById('btn');
 const grid = document.getElementById('grid');
+const score = document.getElementById('score');
+let checkable = true;
+let counter = 0;
 
-buttonPlay.addEventListener('click',
-    function () {
-        switch (difficulty.value) {
-            case 'easy':
-                addElementsDom(10, 10, 1, 100);
-                break;
-            case 'medium':
-                addElementsDom(9, 9, 1, 81);
-                break;
-            case 'hard':
-                addElementsDom(7, 7, 1, 49);
-                break;
-        };
-    }
-);
-
-/*
- *
- * FUNCTIONS
- *
+/**
+ * 
+ * @param   max  max grid's number
+ * @returns      random numbers
  */
+ function getRandomNumber(max) {
+    return random = Math.floor(Math.random() * (max - 1)) + 1;
+}
 
-function addElementsDom(col, row, min, max) {
-    let numberSquare = col * row;
+
+/**
+ * 
+ * @param   max    max grid's number
+ * @param   bombs  bombs' number
+ * @returns        bombs' array
+ */
+ function generateArray(max, bombs) {
+    let arrayBombs = [];
+    for (let index = 1; index <= bombs; index++) {
+        let numberRand = getRandomNumber(max);
+        while (arrayBombs.includes(numberRand)) { 
+            numberRand = getRandomNumber(max);
+        }
+        arrayBombs.push(numberRand);
+    }
+
+    return arrayBombs;
+};
+
+
+/**
+ * 
+ * @param   col    columns and rows' number
+ * @param   max    max grid's number
+ * @param   bombs  bombs' number
+ * @returns        total number of squares
+ */
+function addElementsDom(col, max, bombs) {
+    let numberSquare = Math.pow(col, 2);
     grid.innerHTML = '';
+    score.innerHTML = 0;
+    counter = 0;
 
-    let array = generateArray(min, max);
-    console.log(array);
+    let array = generateArray(max, bombs);
 
     for (let index = 1; index <= numberSquare; index++) {
         let square = document.createElement('div');
         square.classList.add('square');
         square.style.width = `calc(100% / ${col})`;
-        square.style.height = `calc(100% / ${row})`;
+        square.style.height = `calc(100% / ${col})`;
         square.append(index);
         grid.append(square);
 
         square.addEventListener('click',
             function () {
-                const element = this;
-                element.classList.add('square-clicked');
                 if (array.includes(index)) {
-                    element.classList.add('square-clicked-wrong');
+                    if (checkable) {
+                        this.classList.add('square-clicked-wrong');
+                    }
+                    checkable = false;
+
                     setTimeout( () => {
-                        const popUp = document.querySelector('.pop-up');
-                        popUp.classList.add('pop-up-clicked');
-                    }, 250);
-                    console.log('Hai perso!');
-                    let punteggioDom = document.querySelector('p.punteggio');
-                    punteggioDom.append(` punti.`); //manca punteggio effettivo
+                        document.querySelector('.pop-up').classList.add('pop-up-clicked');
+                    }, 150);
                 } else {
-                    element.classList.add('square-clicked-right');
-                };
+                    if (checkable) {
+                        this.classList.add('square-clicked-right');
+                        counter += 1;
+                    }
+                    score.innerHTML = counter;
+                }
             }
         );
-    };
+    }
 
     return numberSquare;
-};
+}
 
-function generateArray(min, max) {
-    let bombs = [];
-    for (let index = 1; index <= 16; index++) {
-        let numberRand = getRandomNumber(min, max);
-        while (bombs.includes(numberRand)) { 
-            numberRand = getRandomNumber(min, max);
-        };
-        bombs.push(numberRand);
-    };
 
-    return bombs;
-};
-
-function getRandomNumber(min, max) {
-    return random = Math.floor(Math.random() * (max - min)) + min;
-};
+buttonPlay.addEventListener('click',
+    function () {
+        switch (difficulty.value) {
+            case 'easy':
+                addElementsDom(10, 100, 5);
+                break;
+            case 'medium':
+                addElementsDom(9, 81, 10);
+                break;
+            case 'hard':
+                addElementsDom(6, 36, 15);
+                break;
+        }
+    }
+);
